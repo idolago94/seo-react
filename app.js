e295b6ac77a1e5e5249d99e4b73361129f071b48
Ido Lago <idol@moonsite.co.il>
2020-09-10 16:39:52 +0300

    route for display headers

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/headers', (req, res, next) => {
+  console.log('request', req);
+  res.json(req.rawHeaders)
+});
 app.use('/', express.static(path.join(__dirname, 'public', 'build')));
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
