6e867e196c57321ddf30672217d5cd0e7b38f1e3
idol <idol@moonsite.co.il>
2021-04-06 18:14:02 +0300

    react app with totomoonsite owner

---
+++ public/build/precache-manifest.954e8fc62a8e22d34740f2f024989ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db277e66fa74657324f874d0ec1ceae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3183a4c756c10e59bb1",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "29ef7cdb6e2413dfb3d1",
+    "url": "/static/js/2.e77d1e56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e77d1e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3183a4c756c10e59bb1",
+    "url": "/static/js/main.c4378d90.chunk.js"
+  },
+  {
+    "revision": "7cf05919a93175537d34",
+    "url": "/static/js/runtime-main.7ab5bd73.js"
+  }
+]);
